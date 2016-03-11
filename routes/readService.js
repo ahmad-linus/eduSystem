c8fc81951974950806acca277afb5ffc6b140061
linus <aam72linus@gmail.com>
2016-03-11 15:22:08 +0330

    fetch completed

--- routes/readService.js
+++ routes/readService.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-router.get('/read/:collection/:id', function(req, res, next){
+router.get('/:collection/:id', function(req, res, next){
     var item;
     var url = 'mongodb://localhost:27017/test';
 
@@ -16,21 +16,23 @@ router.get('/read/:collection/:id', function(req, res, next){
     var fetch = function(db, callback){
         var collectionName = req.params.collection;
         var id = req.params.id;
-        var collection = db.get().collection(collectionName);
-        item = collection.find({"_id" : id});
+        var  obj_id = new require('mongodb').ObjectID(req.params.id);
+        var cursor = db.collection(collectionName).find({_id : obj_id});
+        cursor.each(function(err, doc) {
+            assert.equal(err, null);
+            if (doc != null) {
+                res.json(doc);
+            } else {
+            }
+        });
     };
 
     MongoClient.connect(url, function(err, db){
         assert.equal(null, err);
-        fetch(db, function(){
-            db.close();
-        });
+        fetch(db);
     });
 
-    res.json(item);
 });
-
-
 
 
 module.exports = router;
