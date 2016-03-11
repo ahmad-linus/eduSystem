414038dd6fdaf0741a80e0467b0e6175f5001643
linus <aam72linus@gmail.com>
2016-03-11 15:46:02 +0330

    fetch course by semester, fetch functionality extended

--- routes/readService.js
+++ routes/readService.js
@@ -7,22 +7,15 @@ router.get('/:collection/:id', function(req, res, next){
     var item;
     var url = 'mongodb://localhost:27017/test';
 
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server.");
-        db.close();
-    });
-
     var fetch = function(db, callback){
         var collectionName = req.params.collection;
-        var id = req.params.id;
         var  obj_id = new require('mongodb').ObjectID(req.params.id);
-        var cursor = db.collection(collectionName).find({_id : obj_id});
+        var cursor = db.collection(collectionName).find({"_id" : obj_id});
+
         cursor.each(function(err, doc) {
             assert.equal(err, null);
             if (doc != null) {
                 res.json(doc);
-            } else {
             }
         });
     };
@@ -32,6 +25,22 @@ router.get('/:collection/:id', function(req, res, next){
         fetch(db);
     });
 
+});
+
+router.get('/courses/:semester', function(req, res){
+    var fetchCoursesBySemester = function(db, callback){
+        var semester = req.params.semester;
+        var cursor = db.collection('course').find({"semester" : semester});
+        var courses = [];
+        cursor.each(function (err, doc) {
+            assert.equal(err, null);
+            if (doc != null) {
+                courses.push(doc);
+            }
+        });
+
+        res.json(courses);
+    }
 });
 
 
--- routes/writeService.js
+++ routes/writeService.js
@@ -8,12 +8,6 @@ var assert = require('assert');
 
 router.get('/:collection',  function(req, res){
     var url = 'mongodb://localhost:27017/test';
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server.");
-        db.close();
-    });
-
 
     var insert = function (db, callback) {
         var collectionName = req.params.collection;
