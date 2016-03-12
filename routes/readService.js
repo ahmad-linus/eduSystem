3e99d2d74aa4a07c94981dae2b467b9eda4e3392
linus <aam72linus@gmail.com>
2016-03-12 12:07:36 +0330

    changes to web services refactoring

--- routes/readService.js
+++ routes/readService.js
@@ -3,10 +3,17 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-router.get('/:collection/:id', function(req, res, next){
-    var item;
-    var url = 'mongodb://localhost:27017/test';
 
+var url = 'mongodb://localhost:27017/test';
+
+var dbConnector = function(func){
+    MongoClient.connect(url, function(err, db){
+       assert.equal(null, err);
+       func(db);
+    });
+};
+
+router.get('/:collection/:id', function(req, res, next){
     var fetch = function(db, callback){
         var collectionName = req.params.collection;
         var  obj_id = new require('mongodb').ObjectID(req.params.id);
@@ -14,48 +21,68 @@ router.get('/:collection/:id', function(req, res, next){
 
         cursor.each(function(err, doc) {
             assert.equal(err, null);
-            if (doc != null) {
+            if (doc != null)
                 res.json(doc);
-            }
         });
     };
-
-    MongoClient.connect(url, function(err, db){
-        assert.equal(null, err);
-        fetch(db);
-    });
-
+    dbConnector(fetch);
 });
 
-router.get('/courses/:semester', function(req, res){
+router.get('/courses/semesters/:semester', function(req, res){
     var fetchCoursesBySemester = function(db, callback){
         var semester = req.params.semester;
         var cursor = db.collection('course').find({"semester" : semester});
         var courses = [];
+        console.log(cursor);
         cursor.each(function (err, doc) {
             assert.equal(err, null);
             if (doc != null)
                 courses.push(doc);
         });
-
         res.json(courses);
-    }
+    };
+    dbConnector(fetchCoursesBySemester);
 });
 
-router.get('/courses/:studentID', function(req, res){
+router.get('/courses/student/:studentID', function(req, res){
    var fetchCoursesByStudentID = function(db, callback){
        var stdId = req.params.studentID;
        var dbCursor = db.collection('course').find({"students.StudentID" : stdId});
        var courses = [];
-       cursor.each(function (err, doc){
+       dbCursor.each(function (err, doc){
           assert.equal(err, null);
            if (doc != null)
                 courses.push(doc);
        });
-   }
+       res.json(courses);
+   };
+   dbConnector(fetchCoursesByStudentID);
 });
 
+router.get('/courses/student/:studentID/semester/:semester', function(req, res){
+    var fetchCoursesByStudentIDAndSemester = function(db, callback){
+        var stdId = req.params.studentID;
+        var semester = req.params.semester;
+        var queryJSON = {};
 
+        if (stdId != -1)
+            queryJSON['students.StudentID'] = parseInt(stdId);
 
+        if (semester != -1)
+            queryJSON['semester'] = semester;
+
+        console.log(queryJSON);
+        var dbCursor = db.collection('names').find();
+        var courses = [];
+
+        dbCursor.each(function(err, doc){
+            assert(err, null);
+            if (doc != null)
+                courses.push(doc);
+        });
+        res.json(courses);
+    };
+    dbConnector(fetchCoursesByStudentIDAndSemester);
+});
 
 module.exports = router;
